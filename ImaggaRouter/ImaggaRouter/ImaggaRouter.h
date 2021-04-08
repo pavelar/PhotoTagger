ee2134ce0d441e462ce24d1e7e52a77afa7b1ad5
pavelar <pavelar@advsolutionmpros.com>
2021-04-08 18:34:28 -0500

    Add ImaggeRouter dependency

---
+++ ImaggaRouter/ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,18 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by Phillip Avelar on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
